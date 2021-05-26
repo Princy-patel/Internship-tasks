d420971e0d05924a4af47153ceb3e250cde4fab3
Princy-patel <82884751+Princy-patel@users.noreply.github.com>
2021-05-26 19:28:20 +0530

    Day-2 Task
    Variables and If-else program Using PHP

---
+++ Day2/Variable_day2.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    
+</head>
+<body>
+    <h2>
+        <?php
+            $variable = 100;
+            $variable_1 = "Hello World!";
+            $variable123 = "100.5";
+
+            echo 'The value is  $variable'. "<br>"; //Single quotes:
+            echo "The value is  $variable_1". "<br>";
+            echo "The value is ". $variable123. "<br>";
+
+            $variable = 500;
+            echo "The value is ". $variable. "<br>";
+
+        ?>
+    </h2>
+</body>
+</html>
