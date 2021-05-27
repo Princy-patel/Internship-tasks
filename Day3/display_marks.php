27e7d406867eacf4a722eea40d9aa9b8bd7819cd
Princy Patel <82884751+Princy-patel@users.noreply.github.com>
2021-05-27 23:55:41 +0530

    Display Student Marks
    Display Student Marks Using IF-else Condition

--- Day3/display_marks.php
+++ Day3/display_marks.php
@@ -1,55 +1,68 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Display Marks</title>
+</head>
+<body>
+    <h2>
+        <?php
 
-    // PHP program to enter the marks of five subjects and calculate total, average, and percentage
+        // PHP program to enter the marks of five subjects and calculate total, average, and percentage
 
-        $p = 95;
-        $c = 85;
-        $m = 74;
-        $e = 64;
-        $h = 53;
-        $total = NULL;
-        $average = NULL;
-        $percentage = NULL;
+            $p = 95;
+            $c = 85;
+            $m = 74;
+            $e = 64;
+            $h = 53;
+            $total = NULL;
+            $average = NULL;
+            $percentage = NULL;
 
-    // p, c, m, e, and h are the five subjects
-    // p = physics
-    // c = chemistry
-    // m = math
-    // e = english
-    // h = history
+        // p, c, m, e, and h are the five subjects
+        // p = physics
+        // c = chemistry
+        // m = math
+        // e = english
+        // h = history
 
-    // Calculate total, average and percentage
-        $total = $p + $c + $m + $e + $h;
-        $average = $total / 5.0;
-        $percentage = ($total / 500.0) * 100;
+        // Calculate total, average and percentage
+            $total = $p + $c + $m + $e + $h;
+            $average = $total / 5.0;
+            $percentage = ($total / 500.0) * 100;
 
-    // Output
-        echo "The Total marks   = " . $total . "/500\n"."<br>";
-        echo "The Average marks = " . $average . "\n"."<br>";
-        echo "The Percentage    = " . $percentage . "%"."<br>";
+        // Output
+            echo "The Total marks   = " . $total . "/500\n"."<br>";
+            echo "The Average marks = " . $average . "\n"."<br>";
+            echo "The Percentage    = " . $percentage . "%"."<br>";
+            echo "Your Grade:" . "<br>";
 
-    //  If-else Codition to display Grade
+        //  If-else Codition to display Grade
 
-        if($total >= 450 && $total <= 500)
-        {
-            echo "You have grade A";
-        }elseif($total >= 435 && $total <= 449){
-            echo "You have grade B+";
-        }
-        elseif($total >= 400 && $total <= 434){
-            echo "You have grade B";
-        }
-        elseif($total >= 300 && $total <= 385){
-            echo "You have grade C+";
-        }
-        elseif($total >= 350 && $total <= 384){
-            echo "You have grade C";
-        }
-        else if($total >= 300 && $total <= 349){
-            echo "You have grade D";
-        }
-        else{ 
-            echo "You have grade F";
-        }
-
-?>
+            if($total >= 450 && $total <= 500)
+            {
+                echo "You have grade A";
+            }elseif($total >= 435 && $total <= 449){
+                echo "You have grade B+";
+            }
+            elseif($total >= 400 && $total <= 434){
+                echo "You have grade B";
+            }
+            elseif($total >= 300 && $total <= 385){
+                echo "You have grade C+";
+            }
+            elseif($total >= 350 && $total <= 384){
+                echo "You have grade C";
+            }
+            else if($total >= 300 && $total <= 349){
+                echo "You have grade D";
+            }
+            else{ 
+                echo "You have grade F";
+            }
+        ?>
+    </h2>
+</body>
+</html>
